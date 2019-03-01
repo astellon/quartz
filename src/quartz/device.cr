c427ce4e281df100ba03101825e34030c50bbf39
astellon <astellon2525otastepper@gmail.com>
2019-03-01 11:27:46 +0900

    comment

--- src/quartz/device.cr
+++ src/quartz/device.cr
@@ -1,5 +1,13 @@
 module Quartz
+  # Store the information of a sound device.
   class Device
+    getter index
+    getter name
+    getter host
+    getter input
+    getter output
+    getter sample_rate
+
     def initialize(@index : LibPortAudio::PaDeviceIndex)
       @device = LibPortAudio.get_device_info(index)
       @name = String.new @device.value.name
@@ -9,23 +17,23 @@ module Quartz
       @sample_rate = @device.value.default_sample_rate.as(Float64)
     end
 
-    # return the number of the audio devices
+    # Return the number of the available sound devices.
     def self.ndevices
       LibPortAudio.get_device_count
     end
 
-    # return an array of the audio devices
+    # Return an array of the available sound devices.
     def self.devices
       devices = Array(LibPortAudio::PaDeviceInfo).new
       (0..ndevices - 1).map { |dev| Device.new(dev) }
     end
 
-    # return the index of default input device
+    # Return the index of default input device.
     def self.default_input
       Device.new LibPortAudio.get_default_input_device
     end
 
-    # return the index of default output device
+    # Return the index of default output device.
     def self.default_output
       Device.new LibPortAudio.get_default_output_device
     end
