48f8c959a92aa0eb0c0c38c865ff1388e28459e9
astellon <astellon2525otastepper@gmail.com>
2019-03-03 17:14:31 +0900

    more doc

--- src/quartz/util.cr
+++ src/quartz/util.cr
@@ -1,22 +1,30 @@
 module Quartz
-  # type to pa format
-  # ```
-  # format(Int32) # => 2
-  # ```
-  macro format(type)
-    LibPortAudio::Pa{{type}}
-  end
-
+  # Initialize Quartz module.
+  #
+  # Quartz is **AUTOMATICALLY** initialized when you include this module, so you don't need to call this.
   def self.init
     Suppressor.suppress Process::ORIGINAL_STDERR, LibPortAudio.init
     at_exit { LibPortAudio.terminate }
   end
 
-  def pa_version(io = STDOUT)
+  # Print out the version of internal PortAudio library.
+  def self.pa_version(io = STDOUT)
     io.puts String.new(LibPortAudio.get_version_text)
   end
 
+  # exec PortAudio function and handle PaError.
+  #
+  # Use for functions that return PaError.
   macro except(code)
     ( errno = {{code}} ) < 0 ? raise String.new(LibPortAudio.get_error_text(errno)) : errno
+  end
+
+  # type to pa format
+  #
+  # ```
+  # format(Int32) # => 2
+  # ```
+  macro format(type)
+    LibPortAudio::Pa{{type}}
   end
 end
